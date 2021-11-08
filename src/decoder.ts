575b595ef9a9ef4cc85c30a28b59d214b5dcee38
Satish Babariya <satish.babariya@gmail.com>
2021-11-08 18:48:19 +0530

    added parsing of proto value to object

--- src/decoder.ts
+++ src/decoder.ts
@@ -18,9 +18,9 @@ export function DecodeValue(value: Value): any {
       return null;
 
     case Value.TypeCase.ARRAY_VALUE:
-      var arr = value.getArrayValue();
+      let arr = value.getArrayValue();
       if (arr) {
-        var array = arr.getValuesList().map((value) => {
+        let array = arr.getValuesList().map((value) => {
           return DecodeValue(value);
         });
         return array;
@@ -28,12 +28,58 @@ export function DecodeValue(value: Value): any {
       return [];
 
     case Value.TypeCase.MAP_VALUE:
-      var obj = value.getMapValue()?.getFieldsMap();
+      let obj = value.getMapValue()?.getFieldsMap();
       if (obj) {
-        return obj;
+        let object: any = {};
+        obj.forEach((value, key) => {
+          object[key] = DecodeValue(value);
+        });
+        return object;
       }
       return {};
+
+    case Value.TypeCase.DATE_VALUE:
+     let date = value.getDateValue();
+     if (date) {
+       return new Date(date.getSeconds() * 1000);
+     }
+
+    case Value.TypeCase.OBJECT_ID_VALUE:
+      let id = value.getObjectIdValue();
+      if (id) {
+        return id.getId();
+      }
+
     default:
-      throw new Error("Unsupported value type");
+      console.error("Unsupported type", TypeToString(value.getTypeCase()));
+  }
+}
+
+export function TypeToString(type: Value.TypeCase) {
+  switch (type) {
+    case Value.TypeCase.TYPE_NOT_SET:
+      return "TYPE_NOT_SET";
+    case Value.TypeCase.DOUBLE_VALUE:
+      return "DOUBLE_VALUE";
+    case Value.TypeCase.STRING_VALUE:
+      return "STRING_VALUE";
+    case Value.TypeCase.MAP_VALUE:
+      return "MAP_VALUE";
+    case Value.TypeCase.ARRAY_VALUE:
+      return "ARRAY_VALUE";
+    case Value.TypeCase.BYTES_VALUE:
+      return "BYTES_VALUE";
+    case Value.TypeCase.BOOL_VALUE:
+      return "BOOL_VALUE";
+    case Value.TypeCase.DATE_VALUE:
+      return "DATE_VALUE";
+    case Value.TypeCase.NULL_VALUE:
+      return "NULL_VALUE";
+    case Value.TypeCase.INTEGER_VALUE:
+      return "INTEGER_VALUE";
+    case Value.TypeCase.OBJECT_ID_VALUE:
+      return "OBJECT_ID_VALUE";
+    default:
+      return "unknown";
   }
 }
