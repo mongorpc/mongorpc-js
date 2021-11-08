4035a1c72d858f5568c9d094c3c50f7360c30a9a
Satish Babariya <satish.babariya@gmail.com>
2021-11-08 20:40:10 +0530

    added list documents query builder

--- src/collection.ts
+++ src/collection.ts
@@ -1,6 +1,11 @@
 import { MongoRPCClient } from "./proto";
 import { Database } from "./database";
 import { Document } from "./document";
+import {
+  ListDocumentsRequest,
+  Sort as ProtoSort,
+} from "./proto/pb/mongorpc_pb";
+import { DecodeValue } from "./decoder";
 
 class Collection {
   private client: MongoRPCClient;
@@ -16,6 +21,91 @@ class Collection {
   public document(id: string): Document {
     return new Document(id, this, this.client);
   }
+
+  public documents(): ListDocumentsRequestBuilder {
+    return new ListDocumentsRequestBuilder(this.client, this.parent, this.name);
+  }
 }
 
-export { Collection };
+interface Sort {
+  field: string;
+  asending: boolean;
+}
+
+class ListDocumentsRequestBuilder {
+  _limit?: number;
+  _skip?: number;
+  _sort?: Sort[];
+
+  private client: MongoRPCClient;
+  private parent: Database;
+  private name: string;
+
+  constructor(client: MongoRPCClient, parent: Database, name: string) {
+    this.client = client;
+    this.parent = parent;
+    this.name = name;
+  }
+
+  public limit(limit: number): ListDocumentsRequestBuilder {
+    this._limit = limit;
+    return this;
+  }
+
+  public skip(skip: number): ListDocumentsRequestBuilder {
+    this._skip = skip;
+    return this;
+  }
+
+  public sort(by: {
+    field: string;
+    asending: boolean;
+  }): ListDocumentsRequestBuilder {
+    const sort: Sort = {
+      field: by.field,
+      asending: by.asending,
+    };
+    if (!this._sort) {
+      this._sort = [];
+    }
+    this._sort.push(sort);
+    return this;
+  }
+
+  public async get(): Promise<any[]> {
+    const request = new ListDocumentsRequest();
+    request.setDatabase(this.parent.name);
+    request.setCollection(this.name);
+    if (this._limit) {
+      request.setLimit(this._limit);
+    }
+    if (this._skip) {
+      request.setSkip(this._skip);
+    }
+    if (this._sort) {
+      for (const sort of this._sort) {
+        const sortField = new ProtoSort();
+        sortField.setField(sort.field);
+        sortField.setAscending(sort.asending);
+        request.addSort(sortField);
+      }
+    }
+
+    try {
+      const response = await this.client.listDocuments(request);
+      const documents = response.getDocuments();
+      if (documents) {
+        let result = documents.getValuesList().map((doc) => {
+          return DecodeValue(doc);
+        });
+        return result;
+      }
+
+      return [];
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export { Collection, ListDocumentsRequestBuilder, Sort };
