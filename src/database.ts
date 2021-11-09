64179c0aa5e78e4af60fbe8d42961e96161a335e
Satish Babariya <satish.babariya@gmail.com>
2021-11-09 13:10:57 +0530

    added list collections rpc call

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,7 @@
 import { MongoRPCClient } from "./proto";
 import { Collection } from "./collection";
+import { ListDocumentsRequest } from "./proto/pb/mongorpc_pb";
+import { DecodeValue } from "./decoder";
 
 class Database {
   private client: MongoRPCClient;
@@ -12,6 +14,28 @@ class Database {
 
   public collection(name: string): Collection {
     return new Collection(name, this, this.client);
+  }
+
+  public async listCollections(): Promise<Collection[]> {
+    const request = new ListDocumentsRequest();
+    request.setDatabase(this.name);
+
+    try {
+      const response = await this.client.listCollections(request);
+      const collections = response.getCollections()
+      if (collections) {
+        let values = collections.getValuesList().map(value => {
+          return DecodeValue(value);
+        });
+        return values.map(value => {
+          return new Collection(value.toString(), this, this.client);
+        });
+      }
+
+      return [];
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
