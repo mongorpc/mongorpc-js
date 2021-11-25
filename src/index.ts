d6a66090b1b757f2c5f10fb157252712a37decf9
Satish Babariya <satish.babariya@gmail.com>
2021-11-25 20:03:55 +0530

    remove old source

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { MongoRPC } from "./mongorpc";
-import { Database } from "./database";
-import { Collection } from "./collection";
-import { Document } from "./document";
-
-export { MongoRPC, Database, Collection, Document };
--- src/proto/index.ts
+++
@@ -1,9 +0,0 @@
-import { GetDocumentRequest, Value } from "./pb/mongorpc_pb";
-import { MongoRPCPromiseClient } from "./pb/mongorpc_grpc_web_pb";
-
-export {
-  MongoRPCPromiseClient,
-  MongoRPCPromiseClient as MongoRPCClient,
-  GetDocumentRequest,
-  Value,
-};
